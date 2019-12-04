168bcfa8df8a1fabca63e90efbf703bfd47dfc00
hhp227 <hong227@naver.com>
2019-12-04 16:36:20 +0900

    Update 2019.12.04

---
+++ app/src/main/java/com/hhp227/knu_minigroup/adapter/PicturePagerAdapter.java
@@ -0,0 +1,52 @@
+package com.hhp227.knu_minigroup.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.bumptech.glide.Glide;
+import com.hhp227.knu_minigroup.R;
+import com.hhp227.knu_minigroup.helper.ZoomImageView;
+
+import java.util.List;
+
+public class PicturePagerAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private List<String> images;
+    private ZoomImageView zoomImageView;
+
+    public PicturePagerAdapter(Context context, List<String> images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        if (layoutInflater == null)
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.image_fullscreen, container, false);
+        zoomImageView = view.findViewById(R.id.ziv_image);
+        String image = images.get(position);
+
+        Glide.with(context).load(image).into(zoomImageView);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
