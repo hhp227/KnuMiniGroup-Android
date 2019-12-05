8e0de37e89806e32bbdd761cc512828dbf5c3040
hhp227 <hong227@naver.com>
2019-12-05 16:52:40 +0900

    Update 2019.12.05

---
+++ app/src/main/java/com/hhp227/knu_minigroup/dto/Profile.java
@@ -0,0 +1,10 @@
+package com.hhp227.knu_minigroup.dto;
+
+public class Profile {
+    String imageId, messageId;
+
+    public Profile(String imageId, String messageId) {
+        this.imageId = imageId;
+        this.messageId = messageId;
+    }
+}
