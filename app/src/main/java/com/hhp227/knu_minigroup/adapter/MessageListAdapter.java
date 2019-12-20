b5ffb705cb440cc1769a4fed973738686759f037
hhp227 <hong227@naver.com>
2019-12-20 13:51:41 +0900

    Update 2019.12.20

--- app/src/main/java/com/hhp227/knu_minigroup/adapter/MessageListAdapter.java
+++ app/src/main/java/com/hhp227/knu_minigroup/adapter/MessageListAdapter.java
@@ -5,8 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.hhp227.knu_minigroup.R;
+import com.hhp227.knu_minigroup.app.EndPoint;
 import com.hhp227.knu_minigroup.dto.MessageItem;
 
 import java.text.SimpleDateFormat;
@@ -17,10 +21,11 @@ public class MessageListAdapter extends BaseAdapter {
     private static final int MSG_TYPE_LEFT = 0;
     private static final int MSG_TYPE_RIGHT = 1;
     private Context context;
+    private ImageView profileImage;
     private LayoutInflater inflater;
     private List<MessageItem> messageItems;
     private String imageId;
-    private TextView message, timeStamp;
+    private TextView name, message, timeStamp;
 
     public MessageListAdapter(Context context, List<MessageItem> messageItems, String imageId) {
         this.context = context;
@@ -48,11 +53,15 @@ public class MessageListAdapter extends BaseAdapter {
         if (inflater == null)
             inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (convertView == null)
-            convertView = inflater.inflate(R.layout.message_item_right, null);
-
+            convertView = inflater.inflate(getItemViewType(position) == MSG_TYPE_RIGHT ? R.layout.message_item_right : R.layout.message_item_left, null);
+        profileImage = convertView.findViewById(R.id.iv_profile_image);
+        name = convertView.findViewById(R.id.tv_name);
         message = convertView.findViewById(R.id.tv_message);
         timeStamp = convertView.findViewById(R.id.tv_timestamp);
+
         MessageItem messageItem = messageItems.get(position);
+        Glide.with(context).load(EndPoint.USER_IMAGE.replace("{IMAGE_ID}", messageItem.getFrom())).apply(new RequestOptions().circleCrop()).into(profileImage);
+        name.setText(messageItem.getName());
         message.setText(messageItem.getMessage());
         timeStamp.setText(new SimpleDateFormat("a h:mm", Locale.getDefault()).format(messageItem.getTime()));
 
