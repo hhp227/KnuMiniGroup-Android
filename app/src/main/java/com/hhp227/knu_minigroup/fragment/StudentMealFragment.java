0a7371b1637c4c2b203f50c0619cd7bc4f8e6d9c
hhp227 <hong227@naver.com>
2021-02-03 19:28:24 +0900

    2021.02.03 Commit

--- app/src/main/java/com/hhp227/knu_minigroup/fragment/StudentMealFragment.java
+++ app/src/main/java/com/hhp227/knu_minigroup/fragment/StudentMealFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.text.Html;
 import android.text.SpannableString;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,9 @@ import com.android.volley.toolbox.StringRequest;
 import com.hhp227.knu_minigroup.R;
 import com.hhp227.knu_minigroup.app.AppController;
 import com.hhp227.knu_minigroup.app.EndPoint;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -26,11 +30,23 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class StudentMealFragment extends Fragment {
+    private static final String KEY_BREAKFAST = "breakfast";
+    private static final String KEY_LAUNCH = "lunch";
+    private static final String KEY_DINNER = "dinner";
+
     private static final String TAG = "학생식당 식단표";
 
-    private TextView[][] mMenuView;
+    private Pair<String, TextView>[] mMenuView;
 
     private int mId;
 
@@ -55,17 +71,17 @@ public class StudentMealFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_dormmeal, container, false);
-        mMenuView = new TextView[][] {
-                { rootView.findViewById(R.id.breakfast_title), rootView.findViewById(R.id.breakfast) },
-                { rootView.findViewById(R.id.lunch_title), rootView.findViewById(R.id.lunch) },
-                { rootView.findViewById(R.id.dinner_title), rootView.findViewById(R.id.dinner) }
+        mMenuView = new Pair[] {
+                new Pair<>(KEY_BREAKFAST, rootView.findViewById(R.id.breakfast)),
+                new Pair<>(KEY_LAUNCH, rootView.findViewById(R.id.lunch)),
+                new Pair<>(KEY_DINNER, rootView.findViewById(R.id.dinner))
         };
         String endPoint = EndPoint.URL_KNU_MEAL.replace("{ID}", String.valueOf(mId));
 
         AppController.getInstance().addToRequestQueue(new StringRequest(Request.Method.GET, endPoint, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                ArrayList<String> arrayList = new ArrayList<>();
+                ArrayList<Pair<String, String>> arrayList = new ArrayList<>();
 
                 try {
                     XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
@@ -81,23 +97,29 @@ public class StudentMealFragment extends Fragment {
                             if (startTag.equals("entry")) {
                                 switch (parser.getAttributeValue(0)) {
                                     case "breakfast":
+                                        arrayList.add(new Pair<>(KEY_BREAKFAST, new SpannableString(Html.fromHtml(readEntry(parser))).toString()));
+                                        break;
+                                    case "breakfast_limited":
+                                        arrayList.add(new Pair<>(KEY_BREAKFAST, limitedMeal(new SpannableString(Html.fromHtml(readEntry(parser))).toString())));
                                         break;
                                     case "lunch":
+                                        arrayList.add(new Pair<>(KEY_LAUNCH, new SpannableString(Html.fromHtml(readEntry(parser))).toString()));
+                                        break;
+                                    case "lunch_limited":
+                                        arrayList.add(new Pair<>(KEY_LAUNCH, limitedMeal(new SpannableString(Html.fromHtml(readEntry(parser))).toString())));
                                         break;
                                     case "dinner":
+                                        arrayList.add(new Pair<>(KEY_DINNER, new SpannableString(Html.fromHtml(readEntry(parser))).toString()));
+                                        break;
+                                    case "dinner_limited":
+                                        arrayList.add(new Pair<>(KEY_DINNER, limitedMeal(new SpannableString(Html.fromHtml(readEntry(parser))).toString())));
                                         break;
                                 }
-                                Log.e(TAG, parser.getPrefix());
                             }
-                            if (startTag.equals("entry"))
-                                Log.e("TEST", parser.getAttributeValue(0));
-                            if (startTag.equals("data"))
-                                arrayList.add(new SpannableString(Html.fromHtml(parser.nextText())).toString());
                         }
                         eventType = parser.next();
                     }
-                    /*for (int i = 0; i < mMenuView.length; i++)
-                        mMenuView[i].setText(arrayList.size() > 0 ? arrayList.get(i) : "등록된 식단이 없습니다.");*/
+                    setTextView(arrayList, groupBy(arrayList));
                 } catch (XmlPullParserException | IOException | IndexOutOfBoundsException e) {
                     e.printStackTrace();
                 }
@@ -109,5 +131,74 @@ public class StudentMealFragment extends Fragment {
             }
         }));
         return rootView;
+    }
+
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    private String readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        String data = null;
+
+        parser.require(XmlPullParser.START_TAG, null, "entry");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            if (parser.getName().equals("data")) {
+                data = readText(parser);
+            }
+        }
+        return data;
+    }
+
+    private String limitedMeal(String text) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (!text.isEmpty())
+            stringBuilder.append("\n");
+        stringBuilder.append("* 특식").append("\n").append(text);
+        return stringBuilder.toString();
+    }
+
+    private void setTextView(ArrayList<Pair<String, String>> arrayList, Map<String, List<Pair<String, String>>> stringListMap) {
+        for (Pair<String, TextView> pair: mMenuView) {
+            pair.second.setText(!arrayList.isEmpty() ? test(pair.first, stringListMap) : "등록된 식단이 없습니다.");
+        }
+
+        Log.e("TEST", stringListMap.toString());
+    }
+
+    private String test(String key, Map<String, List<Pair<String, String>>> stringListMap) {
+        Log.e("TEST", stringListMap.toString() + ", " + key);
+        return "1";
+    }
+
+    public static Map<String, List<Pair<String, String>>> groupBy(ArrayList<Pair<String, String>> arrayList) {
+        Map<String, List<Pair<String, String>>> destination = new LinkedHashMap<>();
+
+        for (Pair<String, String> it : arrayList) {
+            String key = it.first;
+            List<Pair<String, String>> value = destination.get(key);
+            List<Pair<String, String>> list;
+
+            if (value == null) {
+                List<Pair<String, String>> answer = new ArrayList<>();
+                list = answer;
+
+                destination.put(key, answer);
+            } else {
+                list = value;
+            }
+            list.add(it);
+        }
+        return destination;
     }
 }
