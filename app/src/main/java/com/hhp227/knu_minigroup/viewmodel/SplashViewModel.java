a1a4563cdce31babc1a2f16d13e5ffcdaf6e7845
hhp227 <hong227@naver.com>
2022-04-17 21:34:57 +0900

    2022.04.17 Commit

---
+++ app/src/main/java/com/hhp227/knu_minigroup/viewmodel/SplashViewModel.java
@@ -0,0 +1,87 @@
+package com.hhp227.knu_minigroup.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.android.volley.Header;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.hhp227.knu_minigroup.app.AppController;
+import com.hhp227.knu_minigroup.app.EndPoint;
+import com.hhp227.knu_minigroup.dto.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SplashViewModel extends ViewModel {
+    public MutableLiveData<State> mState = new MutableLiveData<>(new State());
+
+    public void connection() {
+        AppController.getInstance().addToRequestQueue(new StringRequest(Request.Method.POST, EndPoint.LOGIN, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    if (!jsonObject.getBoolean("isError")) {
+                        mState.postValue(new State(true, false, null));
+                    } else {
+                        mState.postValue(new State(false, true, "패스워드가 변경되었습니다. 다시 로그인해주세요."));
+                    }
+                } catch (JSONException e) {
+                    mState.postValue(new State(false, false, "학습관리시스템(LMS) 서버에 접속할수 없습니다."));
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mState.postValue(new State(false, false, "네트워크 연결을 확인해주세요."));
+                VolleyLog.e(SplashViewModel.class.getSimpleName(), error.getMessage());
+            }
+        }) {
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                for (Header header : response.allHeaders)
+                    if (header.getName().equals("Set-Cookie") && header.getValue().contains("SESSION_NEWLMS"))
+                        AppController.getInstance().getCookieManager().setCookie(EndPoint.LOGIN, header.getValue());
+                return super.parseNetworkResponse(response);
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                User user = AppController.getInstance().getPreferenceManager().getUser();
+                String knuId = user.getUserId();
+                String password = user.getPassword();
+                Map<String, String> params = new HashMap<>();
+
+                params.put("usr_id", knuId);
+                params.put("usr_pwd", password);
+                return params;
+            }
+        });
+    }
+
+    public static final class State {
+        public boolean isSuccess;
+
+        public boolean isPreferenceClear;
+
+        public String message;
+
+        public State() {
+        }
+
+        public State(boolean isSuccess, boolean isPreferenceClear, String message) {
+            this.isSuccess = isSuccess;
+            this.isPreferenceClear = isPreferenceClear;
+            this.message = message;
+        }
+    }
+}
