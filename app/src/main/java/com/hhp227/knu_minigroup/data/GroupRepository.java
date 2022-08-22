10e827da4b76ff018e06b42ed07c3dfcf60c20d2
hhp227 <hhp227@kortek.co.kr>
2022-08-22 18:41:11 +0900

    2022.08.22 Commit

--- app/src/main/java/com/hhp227/knu_minigroup/data/GroupRepository.java
+++ app/src/main/java/com/hhp227/knu_minigroup/data/GroupRepository.java
@@ -1,14 +1,26 @@
 package com.hhp227.knu_minigroup.data;
 
+import static com.hhp227.knu_minigroup.app.EndPoint.GROUP_IMAGE;
+
 import android.graphics.Bitmap;
+
+import androidx.annotation.NonNull;
 
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.hhp227.knu_minigroup.app.AppController;
 import com.hhp227.knu_minigroup.app.EndPoint;
+import com.hhp227.knu_minigroup.dto.GroupItem;
+import com.hhp227.knu_minigroup.dto.User;
 import com.hhp227.knu_minigroup.helper.Callback;
 import com.hhp227.knu_minigroup.volley.util.MultipartRequest;
 
@@ -18,6 +30,7 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -30,7 +43,7 @@ public class GroupRepository {
 
     }
 
-    public void addGroup(String cookie, Bitmap bitmap, String title, String description, String type, Callback callback) {
+    public void addGroup(String cookie, User user, Bitmap bitmap, String title, String description, String type, Callback callback) {
         callback.onLoading();
         AppController.getInstance().addToRequestQueue(new JsonObjectRequest(Request.Method.POST, EndPoint.CREATE_GROUP, null, new Response.Listener<JSONObject>() {
             @Override
@@ -41,9 +54,9 @@ public class GroupRepository {
                         String groupName = response.getString("GRP_NM");
 
                         if (bitmap != null)
-                            groupImageUpdate(cookie, groupId, groupName, description, bitmap, callback);
+                            groupImageUpdate(cookie, user, groupId, groupName, description, bitmap, type, callback);
                         else {
-                            insertGroupToFirebase(groupId, groupName, description, null);
+                            insertGroupToFirebase(user, groupId, groupName, description, null, type, callback);
                         }
                     }
                 } catch (JSONException e) {
@@ -100,22 +113,75 @@ public class GroupRepository {
 
     }
 
-    public void removeGroup() {
+    public void removeGroup(String cookie, User user, boolean isAdmin, String groupId, String key, Callback callback) {
+        AppController.getInstance().addToRequestQueue(new JsonObjectRequest(Request.Method.POST, isAdmin ? EndPoint.DELETE_GROUP : EndPoint.WITHDRAWAL_GROUP, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if (!response.getBoolean("isError")) {
+                        deleteGroupFromFirebase(user, isAdmin, key, callback);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    callback.onFailure(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e(error.getMessage());
+                callback.onFailure(error);
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
 
+                headers.put("Cookie", cookie);
+                return headers;
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/x-www-form-urlencoded; charset=" + getParamsEncoding();
+            }
+
+            @Override
+            public byte[] getBody() {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("CLUB_GRP_ID", groupId);
+                if (params.size() > 0) {
+                    StringBuilder encodedParams = new StringBuilder();
+                    try {
+                        for (Map.Entry<String, String> entry : params.entrySet()) {
+                            encodedParams.append(URLEncoder.encode(entry.getKey(), getParamsEncoding()));
+                            encodedParams.append('=');
+                            encodedParams.append(URLEncoder.encode(entry.getValue(), getParamsEncoding()));
+                            encodedParams.append('&');
+                        }
+                        return encodedParams.toString().getBytes(getParamsEncoding());
+                    } catch (UnsupportedEncodingException uee) {
+                        throw new RuntimeException("Encoding not supported: " + getParamsEncoding(), uee);
+                    }
+                }
+                return null;
+            }
+        });
     }
 
-    private void groupImageUpdate(String cookie, String groupId, String groupName, String description, Bitmap bitmap, Callback callback) {
+    private void groupImageUpdate(String cookie, User user, String groupId, String groupName, String description, Bitmap bitmap, String type, Callback callback) {
         AppController.getInstance().addToRequestQueue(new MultipartRequest(Request.Method.POST, EndPoint.GROUP_IMAGE_UPDATE, new Response.Listener<NetworkResponse>() {
             @Override
             public void onResponse(NetworkResponse response) {
-                insertGroupToFirebase(groupId, groupName, description, bitmap);
+                insertGroupToFirebase(user, groupId, groupName, description, bitmap, type, callback);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 if (error.networkResponse.statusCode == 302) {
                     // 임시로 넣은코드, 서버에서 왜 이런 응답을 보내는지 이해가 안된다.
-                    insertGroupToFirebase(groupId, groupName, description, bitmap);
+                    insertGroupToFirebase(user, groupId, groupName, description, bitmap, type, callback);
                 } else {
                     callback.onFailure(error);
                 }
@@ -156,6 +222,95 @@ public class GroupRepository {
         });
     }
 
-    private void insertGroupToFirebase(String groupId, String groupName, String description, Object o) {
+    private void insertGroupToFirebase(User user, String groupId, String groupName, String description, Bitmap bitmap, String type, Callback callback) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        Map<String, Boolean> members = new HashMap<>();
+        GroupItem groupItem = new GroupItem();
+        Map<String, Object> childUpdates = new HashMap<>();
+        String key = databaseReference.push().getKey();
+
+        members.put(user.getUid(), true);
+        groupItem.setId(groupId);
+        groupItem.setTimestamp(System.currentTimeMillis());
+        groupItem.setAuthor(user.getName());
+        groupItem.setAuthorUid(user.getUid());
+        groupItem.setImage(bitmap != null ? GROUP_IMAGE.replace("{FILE}", groupId.concat(".jpg")) : EndPoint.BASE_URL + "/ilos/images/community/share_nophoto.gif");
+        groupItem.setName(groupName);
+        groupItem.setDescription(description);
+        groupItem.setJoinType(type);
+        groupItem.setMembers(members);
+        groupItem.setMemberCount(members.size());
+        childUpdates.put("Groups/" + key, groupItem);
+        childUpdates.put("UserGroupList/" + user.getUid() + "/" + key, true);
+        databaseReference.updateChildren(childUpdates);
+        callback.onSuccess(new AbstractMap.SimpleEntry<>(key, groupItem));
+    }
+
+    private void deleteGroupFromFirebase(User user, boolean isAdmin, String key, Callback callback) {
+        final DatabaseReference userGroupListReference = FirebaseDatabase.getInstance().getReference("UserGroupList");
+        final DatabaseReference articlesReference = FirebaseDatabase.getInstance().getReference("Articles");
+        final DatabaseReference groupsReference = FirebaseDatabase.getInstance().getReference("Groups");
+
+        if (isAdmin) {
+            groupsReference.child(key).child("members").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        if (snapshot.getKey() != null) {
+                            userGroupListReference.child(snapshot.getKey()).child(key).removeValue();
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    callback.onFailure(databaseError.toException());
+                }
+            });
+            articlesReference.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    DatabaseReference replysReference = FirebaseDatabase.getInstance().getReference("Replys");
+
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        if (snapshot.getKey() != null) {
+                            replysReference.child(snapshot.getKey()).removeValue();
+                        }
+                    }
+                    articlesReference.child(key).removeValue();
+                    groupsReference.child(key).removeValue();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    callback.onFailure(databaseError.toException());
+                }
+            });
+        } else {
+            groupsReference.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    GroupItem groupItem = dataSnapshot.getValue(GroupItem.class);
+
+                    if (groupItem != null) {
+                        if (groupItem.getMembers() != null && groupItem.getMembers().containsKey(user.getUid())) {
+                            Map<String, Boolean> members = groupItem.getMembers();
+
+                            members.remove(user.getUid());
+                            groupItem.setMembers(members);
+                            groupItem.setMemberCount(members.size());
+                        }
+                    }
+                    groupsReference.child(key).setValue(groupItem);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    callback.onFailure(databaseError.toException());
+                }
+            });
+            userGroupListReference.child(user.getUid()).child(key).removeValue();
+        }
+        callback.onSuccess(true);
     }
 }
--- app/src/main/java/com/hhp227/knu_minigroup/viewmodel/Tab4ViewModel.java
+++ app/src/main/java/com/hhp227/knu_minigroup/viewmodel/Tab4ViewModel.java
@@ -2,50 +2,33 @@ package com.hhp227.knu_minigroup.viewmodel;
 
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.util.Log;
 import android.webkit.CookieManager;
 
-import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.hhp227.knu_minigroup.app.AppController;
 import com.hhp227.knu_minigroup.app.EndPoint;
-import com.hhp227.knu_minigroup.dto.GroupItem;
+import com.hhp227.knu_minigroup.data.GroupRepository;
 import com.hhp227.knu_minigroup.dto.User;
+import com.hhp227.knu_minigroup.helper.Callback;
 import com.hhp227.knu_minigroup.helper.PreferenceManager;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Tab4ViewModel extends ViewModel {
     public Boolean mIsAdmin;
 
     public String mGroupId, mGroupImage, mKey;
 
-    private static final String TAG = Tab4ViewModel.class.getSimpleName(), STATE = "state";
+    private static final String STATE = "state";
 
     private final CookieManager mCookieManager = AppController.getInstance().getCookieManager();
 
     private final PreferenceManager mPreferenceManager = AppController.getInstance().getPreferenceManager();
 
     private final SavedStateHandle mSavedStateHandle;
+
+    private final GroupRepository mGroupRepository = new GroupRepository();
 
     public Tab4ViewModel(SavedStateHandle savedStateHandle) {
         mSavedStateHandle = savedStateHandle;
@@ -68,131 +51,22 @@ public class Tab4ViewModel extends ViewModel {
     }
 
     public void deleteGroup() {
-        AppController.getInstance().addToRequestQueue(new JsonObjectRequest(Request.Method.POST, mIsAdmin ? EndPoint.DELETE_GROUP : EndPoint.WITHDRAWAL_GROUP, null, new Response.Listener<JSONObject>() {
+        mGroupRepository.removeGroup(getCookie(), getUser(), mIsAdmin, mGroupId, mKey, new Callback() {
             @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    if (!response.getBoolean("isError")) {
-                        deleteGroupFromFirebase();
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    mSavedStateHandle.set(STATE, new State(false, false, e.getMessage()));
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.e(TAG, error.getMessage());
-                mSavedStateHandle.set(STATE, new State(false, false, error.getMessage()));
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() {
-                Map<String, String> headers = new HashMap<>();
-
-                headers.put("Cookie", getCookie());
-                return headers;
+            public <T> void onSuccess(T data) {
+                mSavedStateHandle.set(STATE, new State(false, (Boolean) data, "소모임 " + (mIsAdmin ? "폐쇄" : "탈퇴") + " 완료"));
             }
 
             @Override
-            public String getBodyContentType() {
-                return "application/x-www-form-urlencoded; charset=" + getParamsEncoding();
+            public void onFailure(Throwable throwable) {
+                mSavedStateHandle.set(STATE, new State(false, false, throwable.getMessage()));
             }
 
             @Override
-            public byte[] getBody() {
-                Map<String, String> params = new HashMap<>();
-
-                params.put("CLUB_GRP_ID", mGroupId);
-                if (params.size() > 0) {
-                    StringBuilder encodedParams = new StringBuilder();
-                    try {
-                        for (Map.Entry<String, String> entry : params.entrySet()) {
-                            encodedParams.append(URLEncoder.encode(entry.getKey(), getParamsEncoding()));
-                            encodedParams.append('=');
-                            encodedParams.append(URLEncoder.encode(entry.getValue(), getParamsEncoding()));
-                            encodedParams.append('&');
-                        }
-                        return encodedParams.toString().getBytes(getParamsEncoding());
-                    } catch (UnsupportedEncodingException uee) {
-                        throw new RuntimeException("Encoding not supported: " + getParamsEncoding(), uee);
-                    }
-                }
-                return null;
+            public void onLoading() {
+                mSavedStateHandle.set(STATE, new State(true, false, null));
             }
         });
-    }
-
-    private void deleteGroupFromFirebase() {
-        final DatabaseReference userGroupListReference = FirebaseDatabase.getInstance().getReference("UserGroupList");
-        final DatabaseReference articlesReference = FirebaseDatabase.getInstance().getReference("Articles");
-        final DatabaseReference groupsReference = FirebaseDatabase.getInstance().getReference("Groups");
-
-        if (mIsAdmin) {
-            groupsReference.child(mKey).child("members").addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        if (snapshot.getKey() != null) {
-                            userGroupListReference.child(snapshot.getKey()).child(mKey).removeValue();
-                        }
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Log.e(TAG, "파이어베이스 데이터 불러오기 실패", databaseError.toException());
-                    mSavedStateHandle.set(STATE, new State(false, false, databaseError.getMessage()));
-                }
-            });
-            articlesReference.child(mKey).addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    DatabaseReference replysReference = FirebaseDatabase.getInstance().getReference("Replys");
-
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        if (snapshot.getKey() != null) {
-                            replysReference.child(snapshot.getKey()).removeValue();
-                        }
-                    }
-                    articlesReference.child(mKey).removeValue();
-                    groupsReference.child(mKey).removeValue();
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Log.e(TAG, databaseError.getMessage());
-                    mSavedStateHandle.set(STATE, new State(false, false, databaseError.getMessage()));
-                }
-            });
-        } else {
-            groupsReference.child(mKey).addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    GroupItem groupItem = dataSnapshot.getValue(GroupItem.class);
-
-                    if (groupItem != null) {
-                        if (groupItem.getMembers() != null && groupItem.getMembers().containsKey(getUser().getUid())) {
-                            Map<String, Boolean> members = groupItem.getMembers();
-
-                            members.remove(getUser().getUid());
-                            groupItem.setMembers(members);
-                            groupItem.setMemberCount(members.size());
-                        }
-                    }
-                    groupsReference.child(mKey).setValue(groupItem);
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Log.e(TAG, "파이어베이스 데이터 불러오기 실패", databaseError.toException());
-                    mSavedStateHandle.set(STATE, new State(false, false, databaseError.getMessage()));
-                }
-            });
-            userGroupListReference.child(getUser().getUid()).child(mKey).removeValue();
-        }
-        mSavedStateHandle.set(STATE, new State(false, true, "소모임 " + (mIsAdmin ? "폐쇄" : "탈퇴") + " 완료"));
     }
 
     public static final class State implements Parcelable {
@@ -208,7 +82,7 @@ public class Tab4ViewModel extends ViewModel {
             this.message = message;
         }
 
-        protected State(Parcel in) {
+        private State(Parcel in) {
             isLoading = in.readByte() != 0;
             isSuccess = in.readByte() != 0;
             message = in.readString();
