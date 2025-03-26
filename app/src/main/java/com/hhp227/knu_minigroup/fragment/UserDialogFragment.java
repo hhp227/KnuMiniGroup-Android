bfeac184eb4d766a59f506dc88ef3f1e467ae9e1
hhp227 <hong227@naver.com>
2025-03-27 02:07:59 +0900

    2025.03.27 Commit

--- app/src/main/java/com/hhp227/knu_minigroup/fragment/UserDialogFragment.java
+++ app/src/main/java/com/hhp227/knu_minigroup/fragment/UserDialogFragment.java
@@ -10,35 +10,19 @@ import android.view.Window;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.load.model.LazyHeaders;
-import com.bumptech.glide.request.RequestOptions;
+import androidx.lifecycle.ViewModelProvider;
 import com.hhp227.knu_minigroup.activity.ChatActivity;
-import com.hhp227.knu_minigroup.R;
-import com.hhp227.knu_minigroup.app.AppController;
-import com.hhp227.knu_minigroup.app.EndPoint;
 import com.hhp227.knu_minigroup.databinding.FragmentUserBinding;
+import com.hhp227.knu_minigroup.handler.OnFragmentUserDialogEventListener;
+import com.hhp227.knu_minigroup.viewmodel.UserViewModel;
 
-// TODO
-public class UserDialogFragment extends DialogFragment {
-    private String mUid, mName, mValue;
+public class UserDialogFragment extends DialogFragment implements OnFragmentUserDialogEventListener {
+    private UserViewModel mViewModel;
 
     private FragmentUserBinding mBinding;
 
     public static UserDialogFragment newInstance() {
         return new UserDialogFragment();
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mUid = getArguments().getString("uid");
-            mName = getArguments().getString("name");
-            mValue = getArguments().getString("value");
-        }
     }
 
     @NonNull
@@ -54,47 +38,36 @@ public class UserDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mBinding = FragmentUserBinding.inflate(inflater, container, false);
+        mViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         return mBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Glide.with(this)
-                .load(new GlideUrl(EndPoint.USER_IMAGE.replace("{UID}", mUid), new LazyHeaders.Builder().addHeader("Cookie", AppController.getInstance().getCookieManager().getCookie(EndPoint.LOGIN)).build()))
-                .apply(RequestOptions.errorOf(R.drawable.user_image_view_circle)
-                        .circleCrop()
-                        .skipMemoryCache(true)
-                        .diskCacheStrategy(DiskCacheStrategy.NONE))
-                .into(mBinding.ivProfileImage);
-        mBinding.tvName.setText(mName);
-        if (mUid.equals(AppController.getInstance().getPreferenceManager().getUser().getUid()))
-            mBinding.bSend.setVisibility(View.GONE);
-        else {
-            mBinding.bSend.setText("메시지 보내기");
-            mBinding.bSend.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(getContext(), ChatActivity.class);
-                    intent.putExtra("grp_chat", false);
-                    intent.putExtra("chat_nm", mName);
-                    intent.putExtra("uid", mUid);
-                    intent.putExtra("value", mValue);
-                    startActivity(intent);
-                }
-            });
-        }
-        mBinding.bClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserDialogFragment.this.dismiss();
-            }
-        });
+        mBinding.setViewModel(mViewModel);
+        mBinding.setLifecycleOwner(getViewLifecycleOwner());
+        mBinding.setHandler(this);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         mBinding = null;
+    }
+
+    @Override
+    public void onSendClick() {
+        Intent intent = new Intent(getContext(), ChatActivity.class);
+        intent.putExtra("grp_chat", false);
+        intent.putExtra("chat_nm", mViewModel.mName);
+        intent.putExtra("uid", mViewModel.mUid);
+        intent.putExtra("value", mViewModel.mValue);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onCancelClick() {
+        UserDialogFragment.this.dismiss();
     }
 }
---
+++ app/src/main/java/com/hhp227/knu_minigroup/handler/OnFragmentUserDialogEventListener.java
@@ -0,0 +1,7 @@
+package com.hhp227.knu_minigroup.handler;
+
+public interface OnFragmentUserDialogEventListener {
+    void onSendClick();
+
+    void onCancelClick();
+}
